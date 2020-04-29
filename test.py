2a26e33fae9ec1bf2ad649e24f6d256d8813ef0b
sandcobainer <sdasari38@gatech.edu>
2020-04-29 18:42:06 -0400

    interpolation and measure calculation

--- test.py
+++ test.py
@@ -25,14 +25,18 @@ TODO  Live coding practice:
 
 p1 >> pads(degree=[4,6,7,2], dur=4, amp=0.5, chop=2, delay=0.5)
 p2 >> pads(degree=[3,1,2,5], dur=0.5, amp=0.7, chop=8, delay=0.2)
-evolve(p1, p2, stepSize = 0.5, evolutions : 3000)
+evolve(p1, p2, stepSize = 0.2, skipGenerations= 2, evolutions = 20, interpolate = linvar)
 
 @nextBar
 def autoEvolution():
-	p1>>pads(degree=[3,1,2,5],dur=[1],amp=[0.5],chop=[8.5],delay=[0])
+	p1>>pads(degree=[3.6,5.8,7,2.4],dur=linvar([4.6,4.4],17.6),amp=linvar([0,0],17.6),chop=linvar([2.4,2.4],17.6),delay=linvar([0.8,0.8],17.6))
 autoEvolution()
 
+p1 >> pads([4,6,7,2], dur= [1,1,3,1], amp=0.5, chop=2, delay=0.5)
+p2 >> pads([5,6,7,2], dur= [1,2,3,4], amp=0.7, chop=8, delay=0.2)
+evolve(p1, p2, stepSize = 0.2, skipGenerations= 5, lifetime = 2000, evolutions = 20, interpolate = expvar)
+
 @nextBar
 def autoEvolution():
-	p1>>pads(degree=[3,1,1,5],dur=[0.5],amp=[1],chop=[8],delay=[1])
+	p1>>pads(degree=[4,6,6.8,2],dur=expvar([0.8,0.6],2.4),amp=expvar([0.8,0.6],2.4),chop=expvar([2.4,2.8],2.4),delay=expvar([0.2,0],2.4))
 autoEvolution()
