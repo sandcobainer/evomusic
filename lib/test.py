299e0873633548af53048d6195e290cf4ef3994b
sandcobainer <sdasari38@gatech.edu>
2020-08-17 00:38:16 -0500

    interpolation broken, fixed stop and feedback

--- lib/test.py
+++ lib/test.py
@@ -1,14 +1,3 @@
-p1>>pluck(amp=0.2)
-p2>>pluck(amp=0.5)
-evolve(p1,p2)
-
-@nextBar
-def evolve_p1_p2():
-	p1>>pluck(amp=[0.55])
-evolve_p1_p2()
-########## Evolving from p1 to p2
-@nextBar
-def evolve_p1_p2():
-	p1>>pluck(amp=[0.54])
-evolve_p1_p2()
-########## Evolving from p1 to p2
+p1>>pluck(room=0.1, amp=0.2,lpf=500)
+p2>>pluck(room=0.8, amp=0.5,lpf=5000)
+evolve(p1,p2,lifetime=5000)
